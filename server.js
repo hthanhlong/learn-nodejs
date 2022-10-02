322a2d5c34b82a2d480c562b9a1b8098cdf09b3a
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-10-02 22:51:08 +0700

    add sequelize

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+const connectDB = require("./src/config/connectDb");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// connect DB
+connectDB();
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => console.log("app listen on port ====> ", port));
