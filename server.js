2a271c9b54b23adc8eebd5efdce36afa9329e2ca
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-10-10 14:06:47 +0700

    add handle error

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
 const app = require("./src/app");
 const port = 5000;
-
-// Error Handling Middleware called
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
-
 app.listen(port, () => console.log("app listen on port ====> ", port));
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ dotenv.config();
 const helmet = require("helmet");
 const morgan = require("morgan");
 const compression = require("compression");
+const createError = require("http-errors");
 
 // connect DB
 connectDB();
@@ -27,5 +28,21 @@ app.use(
 
 //router
 app.use("/api", require("./routes/index"));
+
+// handle - Not Found
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 404,
+    message: " Not Found",
+  });
+});
+
+// handle error from server
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    status: err.status || 500,
+    message: err.message || "",
+  });
+});
 
 module.exports = app;
