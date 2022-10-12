e784c3ac046b5126b0f14792b7556b4a00c753c6
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-10-12 11:11:03 +0700

    update code

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Schedule extends Model {
     /**
@@ -9,19 +7,22 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate() {
       // define association here
     }
   }
-  Schedule.init({
-    currentNumber: DataTypes.INTEGER,
-    maxNumber: DataTypes.INTEGER,
-    date: DataTypes.DATE,
-    timeType: DataTypes.STRING,
-    doctorId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Schedule',
-  });
-  return Schedule;
-};
+  Schedule.init(
+    {
+      currentNumber: DataTypes.INTEGER,
+      maxNumber: DataTypes.INTEGER,
+      date: DataTypes.DATE,
+      timeType: DataTypes.STRING,
+      doctorId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Schedule',
+    }
+  )
+  return Schedule
+}
