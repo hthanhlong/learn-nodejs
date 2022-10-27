1162839fbec9f485b50974b7429f3ec912d136a6
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-10-27 14:51:13 +0700

    update script

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import express from 'express'
 import cors from 'cors'
-import connectDB from './src/config/connectDb.js'
+import connectDB from './config/connectDb.js'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import compression from 'compression'
-import rootRouter from './src/routers/index.js'
+import rootRouter from './routers/index.js'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
